2d275bdf086f21009a4c8c0e641ee39acd5a83d4
aavnip <aavnip@chicagobooth.edu>
2022-02-15 16:18:19 +0000

    Defined characters

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,4 +11,12 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
+  def characters 
+    my_character_id = self.id
+    matching_characters = Character.where({:actor_id => my_character_id})
+    return matching_characters
+  end
 end
+
+# get my id, then characters, 
+# for each character get the movie, then return all the movies
--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,12 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+
+  def movies
+    my_movie_id = self.movie_id
+    matching_movies = Movie.where({:id => my_movie_id})
+    the_movie = matching_movies.at(0)
+    return the_movie
+  end
+
 end
